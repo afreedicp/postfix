7352a404f60c6987d7da7c6047a8f46153fa4c97
afreedicp <afreedi@hamon.in>
2022-03-14 15:39:49 +0530

    green : test condition for more operators than opperent have set

--- postfixOperation.py
+++ postfixOperation.py
@@ -1,5 +1,14 @@
 def postfix_operator(string):
     list = []
+    operatorlist = []
+    charlist = []
+    for i in string:
+        if i == '+' or i == '*' or i == '/' or i == '-':
+            operatorlist.append(i)
+        if i.isdigit():
+            charlist.append(i)
+        if(len(charlist) < len(operatorlist)):
+            return "anable to operate"
     for i in string:
         if i.isdigit():
             list.append(int(i))
--- test_post.py
+++ test_post.py
@@ -4,3 +4,8 @@ import postfixOperation
 def testnonoperator():
     ret = postfixOperation.postfix_operator("1234")
     assert ret == [1, 2, 3, 4]
+
+
+def testonly_operents():
+    ret = postfixOperation.postfix_operator("/*-+*-/-/-+")
+    assert ret == "anable to operate"
